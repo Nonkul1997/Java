1ff4576f369f402264f9a11b235de21c8ede7c07
Nonkul1997 <1165534413@qq.com>
2018-11-14 22:15:59 +0800

    rm

--- Git/src/com/edu/git/test/Xiaochuan.java
+++
@@ -1,9 +0,0 @@
-package com.edu.git.test;
-
-public class Xiaochuan {
-	
-	public static void main(String[] args) {
-		System.out.println("我是是肖川");
-		System.out.println("Git tracks changes of files");
-	}
-}
