217e9f80909b5abcb2f7b1fb71e87a4d695662e5
Nonkul1997 <1165534413@qq.com>
2018-11-14 20:55:54 +0800

    new Java project

---
+++ Git/src/com/edu/git/test/Xiaochuan.java
@@ -0,0 +1,9 @@
+package com.edu.git.test;
+
+public class Xiaochuan {
+	
+	public static void main(String[] args) {
+		System.out.println("Œ“ «–§¥®");
+		System.out.println("Git tracks changes of files");
+	}
+}
